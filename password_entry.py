b9c0556bf919ee3fb69d2746c371a1577707d9bf
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2018-08-14 12:10:36 +1000

    Add a password checking program.

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,14 @@
 """"Matthew Rooke"""
+
+MIN_LENGTH = 5
+
+
+def main():
+    password = input("Enter your password:")
+    while len(password) < MIN_LENGTH:
+        password = input("Enter a password {} characters long:".format(MIN_LENGTH))
+    for character in range(len(password)):
+        print("*", end="")
+
+
+main()
